e9176032b2cb9acb911bbfa2096016488f996dc8
D2D <cmstester6@gmail.com>
2026-01-08 11:04:08 +0530

    Vite change

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,4 +10,8 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    host: '0.0.0.0',  // Ensure Vite binds to all available network interfaces
+    port: 5173,        // Make sure this matches the port Render is expecting
+  },
 })
