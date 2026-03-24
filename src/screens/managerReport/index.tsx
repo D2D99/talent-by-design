63840ee05c9117692f422277b8728950d70196a3
Manik Gupta <cmstester6@gmail.com>
2026-03-24 16:53:26 +0530

    fixes

--- src/screens/managerReport/index.tsx
+++ src/screens/managerReport/index.tsx
@@ -508,7 +508,7 @@ const ManagerReport = () => {
               type="button"
               onClick={handleExportPDF}
               disabled={exportLoading}
-              className="relative hidden overflow-hidden z-0 text-[var(--white-color)] ps-2.5 pe-5 h-10 rounded-full flex justify-center items-center gap-1.5 font-semibold text-base uppercase bg-gradient-to-r from-[#1a3652] to-[#448bd2] duration-200 hover:before:scale-x-100 before:content-[''] before:absolute before:inset-0 before:bg-[#448cd2]/30 before:origin-bottom-left before:scale-x-0 before:transition-transform before:duration-300 before:ease-out before:-z-10"
+              className="relative overflow-hidden z-0 text-[var(--white-color)] ps-2.5 pe-5 h-10 rounded-full flex justify-center items-center gap-1.5 font-semibold text-base uppercase bg-gradient-to-r from-[#1a3652] to-[#448bd2] duration-200 hover:before:scale-x-100 before:content-[''] before:absolute before:inset-0 before:bg-[#448cd2]/30 before:origin-bottom-left before:scale-x-0 before:transition-transform before:duration-300 before:ease-out before:-z-10"
               style={{ backgroundColor: "#1a3652" }}
             >
               {exportLoading ? (
